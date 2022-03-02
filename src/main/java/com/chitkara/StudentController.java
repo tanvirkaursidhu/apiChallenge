b970d12d7bc4bef3ed75aa359ae94aed3b8a714a
TANVIRSIDHU25 <tanvirksidhu@gmail.com>
2022-03-02 14:49:36 +0530

    changed route to /bfhl
    changed route to /bfhl

--- src/main/java/com/chitkara/StudentController.java
+++ src/main/java/com/chitkara/StudentController.java
@@ -1,10 +1,6 @@
 package com.chitkara;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,18 +10,7 @@ public class StudentController {
 	@Autowired
 	private IStudentService studentService;
 
-	@GetMapping(value = "/student")
-	public Student getStudentData() {
-		List<String> input = new ArrayList<String>();
-		input.add("A");
-		input.add("D");
-		input.add("1");
-		input.add("6");
-		input.add("7");
-		return studentService.createStudentData(input);
-	}
-
-	@PostMapping(value = "/student")
+	@PostMapping(value = "/bfhl")
 	public Student createStudentData(@RequestBody RequestJson requestJson) {
 		return studentService.createStudentData(requestJson.getData());
 	}
